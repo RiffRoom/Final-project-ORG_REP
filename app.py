04a534f5dbdebd75a270633d4ce4f48aba1bfceb
Nail Claros <nailclaros@gmail.com>
2023-11-06 11:25:52 -0500

    models for db completed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, abort
-from models import db, Session
+from models import db, Session, sessions, user_table
 from dotenv import load_dotenv
 import os
 
@@ -30,7 +30,22 @@ def homepage():
 
 @app.get('/sessions')
 def get_sessions():
-    active_sessions = Session.query.all()
+    active_sessions = sessions.query.all()
+    print("HASAHSHASHASHASH")
+
+    # us = user_table('Nail', 'Claros', 'ncdash', 'pswd', 'nc@gmail.com', 68968942) ## id 3
+    # db.session.add(us)
+    # db.session.commit()
+
+    # base logic, id must exsist already
+    # s = sessions('Sample Title-2', 'YYYYYYAAA-2', '2023-10-01', 34.121212121313, 35.21324232323232, 3)
+    # db.session.add(s)
+    # db.session.commit()
+
+    # concept for alternate session instantiation
+    # s = sessions.full('Dve Char', 'samp 2', 'smap msggg', '2023-10-01', 34.12121221212, 35,14122323221312, 5)
+    # db.session.add(s)
+    # db.session.commit()
     return render_template('sessions.html', active_sessions=active_sessions)
 
 
