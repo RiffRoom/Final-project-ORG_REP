c8178f0a179c69f5a50ace4d50faaca614d26bc1
Noah Eddleman <noaheddleman@gmail.com>
2023-11-20 11:19:57 -0500

    Updating app.py

--- app.py
+++ app.py
@@ -96,66 +96,6 @@ def homepage():
     return render_template('index.html', videos=videos, distribution_url=distribution_url)    
 
 
-@app.get('/sessions')
-def get_sessions():
-    if not session.get('id'):
-        return redirect('/login')
-
-    MAPS_API_KEY = os.getenv('MAPS_API_KEY') 
-    current_date = datetime.now().strftime('%Y-%m-%dT%H:%M')
-    max_date = datetime(2024, 12, 31,23)
-    active_jam_sessions = JamSession.query.all()
-
-    jam_session_data = []
-
-    for i in active_jam_sessions:
-        result = i.serialize
-        date_str = JamSession.date_str(result['date'])
-        jam_session_data.append(result)
-
-    return render_template('sessions.html', current_date=current_date, max_date=max_date, active_jam_sessions=active_jam_sessions, jam_session_data=jam_session_data, date_str=date_str, MAPS_API_KEY=MAPS_API_KEY)
-
-@app.post('/sessions')
-def add_new_session():
-    data = request.get_json()
-    title = data['title']
-
-    if title is None or title == '':
-        abort(400)
-
-    message = data['message']
-
-    lat = data['lat']
-    lng = data['lng']
-
-    if lat is None or lat == '' or lng is None or lng == '':
-        abort(400)
-
-    date = data['date']
-
-    if date is None or date == '':
-        abort(400)
-
-    date_posted = datetime.now().strftime('%Y-%m-%dT%H:%M')
-
-    s = JamSession(title, message, date, date_posted, lat, lng, 1)
-    db.session.add(s)
-    db.session.commit()
-    return redirect(url_for('get_sessions'))
-
-@app.post('/sessions/<int:jam_session_id>/delete')
-def delete_session(session_id: int):
-    session = JamSession.query.get(session_id)
-    db.session.delete(session)
-    db.session.commit()
-    return redirect(url_for('get_sessions'))
-
-@app.get('/sessions/<int:session_id>')
-def get_single_session(session_id: int):
-    session = JamSession.query.get(session_id)
-    return render_template('get_single_session.html', session=session)
-
-
 @app.route('/user_prof')
 def user_prof():
     return None #rendertemplate('user_profile.html')
@@ -208,8 +148,8 @@ def upload_video():
         file_ext = os.path.splitext(filename)[1]        
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
             abort(400)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-        generate_thumbnail(f'{app.config["UPLOAD_PATH"]}/{filename}', app.config['UPLOAD_PATH'])
+        #uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        #generate_thumbnail(f'{app.config["UPLOAD_PATH"]}/{filename}', app.config['UPLOAD_PATH'])
     return redirect(url_for('get_video'))
 
 @app.get('/login')
