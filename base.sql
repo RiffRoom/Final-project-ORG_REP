1a16567e8ec7f5d314bd6036892f8080bf72b4b1
Noah Eddleman <noaheddleman@gmail.com>
2023-11-25 18:57:37 -0500

    Small changes to session display

--- base.sql
+++ base.sql
@@ -10,12 +10,6 @@ create table user_table (
     phone varchar(20) null,
     prof_pic varchar(255)
 );
-insert into user_table(first_name, last_name, user_name, password, email, phone) values
-('James', 'Bond', 'usern', 'psswd', 'jb@email.com', 1234567890); -- host id 1
-insert into user_table(first_name, last_name, user_name, password, email, phone) values
-('Jack', 'Daniel', 'usern2', 'psswd2', 'jd@email.com', 1234567777); -- host id 2
-insert into user_table (first_name, last_name, user_name, password, email, phone)
-values ('Todd', 'Mclovin', 'us3', 'p3', 'Mc@gmail.com', 1453456788); -- user 3
 
 drop table if exists sessions cascade ;
 create table sessions (
@@ -30,17 +24,6 @@ create table sessions (
     host_id int not null,
     foreign key (host_id) references user_table(id)
 );
--- 35.30559659944004, -80.73257422631464
-insert into sessions(host_name, title, message, date, date_posted, lat, long, host_id) values
-('James Bond', 'Atkins slam', 'Crazy session at Atkins', '2023-03-01 14:12:00', '2023-02-14 14:12:00',35.30559659944004, -80.73257422631464, 1); -- sesh 1
--- 35.30752138310208, -80.73403424319967
-insert into sessions(host_name, title, message, date, date_posted, lat, long, host_id) values
-('Jack Daniel', 'whats up cato', 'Crazy educational session', '2023-03-01 09:15:00', '2023-01-12 14:12:00', 35.30752138310208, -80.73403424319967, 2);-- sesh 2
-
-insert into sessions(host_name, title, message, date, date_posted, lat, long, host_id) values
-('Todd Mclovin', 'MCLOVIN', 'Come on down', '2023-04-16 17:30:00', '2023-03-01 14:12:00', 35.30356435381034, -80.73075445239294, 3); -- sesh 3
-insert into sessions(host_name, title, message, date, date_posted, lat, long, host_id) values
-('Todd Mclovin', 'MCLOVIN PART 2', 'Come on down AGAIN', '2023-04-25 17:30:00', '2023-03-19 14:12:00', 35.30394085324328, -80.73147328440633, 3); -- sesh 3
 
 drop table if exists party cascade ;
 create table party(
@@ -50,10 +33,6 @@ create table party(
     foreign key (session_id) references sessions(id),
     foreign key (user_id) references user_table(id)
 );
-
-insert  into party(session_id, user_id, user_name) values (1, 2, 'Jack Daniel'); -- jack daniel is in james bond's party
-insert  into party(session_id, user_id, user_name) values (2, 1, 'James Bond'); -- james bond is in jack daniel's party
-insert into party(session_id, user_id, user_name) values (1, 3, 'Todd Mclovin'); -- Todd is now in James bond's party
 
 drop table if exists post cascade;
 create table post(
@@ -86,5 +65,4 @@ create table comments(
     post_time timestamp null,
     foreign key (comment_section_id) references  comment_section(id),
     foreign key (commenter_id) references user_table(id)
-
 );
--- blueprints/jam_session/jam_sessions.py
+++ blueprints/jam_session/jam_sessions.py
@@ -8,7 +8,6 @@ load_dotenv()
 
 jam_sessions_bp = Blueprint('jam_sessions', __name__, template_folder='templates')
 
-
 @jam_sessions_bp.get('/')
 def get_sessions():
     if not session.get('id'):
@@ -19,25 +18,23 @@ def get_sessions():
     max_date = datetime(2024, 12, 31,23)
     active_jam_sessions = JamSession.query.all()
 
+    # Used for Google Map
     jam_session_data = []
 
     if active_jam_sessions:
         for i in active_jam_sessions:
             result = i.serialize
-            date_str = JamSession.date_str(result['date'])
             jam_session_data.append(result)
-
 
     return render_template('jam_sessions.html', 
                         current_date=current_date, 
                         max_date=max_date, 
                         active_jam_sessions=active_jam_sessions,
                         jam_session_data=jam_session_data,
-                        date_str=date_str, 
                         MAPS_API_KEY=MAPS_API_KEY,
-                        jam_session=JamSession,
-                        party=Party,
-                        user_table=UserTable)
+                        JamSession=JamSession,
+                        Party=Party,
+                        UserTable=UserTable)
 
 
 
@@ -77,7 +74,7 @@ def add_new_session():
 @jam_sessions_bp.get('/<int:session_id>')
 def get_single_session(session_id: int):
     jam_session = JamSession.query.get(session_id)
-    return render_template('single_session.html', jam_session=jam_session)
+    return render_template('single_session.html', jam_session=jam_session, Party=Party, UserTable=UserTable)
 
 @jam_sessions_bp.post('/<int:session_id>/delete')
 def delete_session(session_id: int):
@@ -94,7 +91,7 @@ def join_session(session_id: int):
         party = Party(jam_session.id, session.get('id'))
         db.session.add(party)
         db.session.commit()
-        flash(f'Joined {jam_session.host_name}s Session!')
+        flash(f"Joined {UserTable.query.get(party.user_id).user_name}'s Session!")
         return redirect(url_for('jam_sessions.get_sessions'))
     else:
         flash('You are already in this session!')
