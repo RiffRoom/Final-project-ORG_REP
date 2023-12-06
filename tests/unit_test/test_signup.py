3fc9b09b3e5872367f483d6411ecbfdd04050c5d
Nail Claros <nailclaros@gmail.com>
2023-12-06 11:26:50 -0500

    test commit

--- tests/unit_test/test_signup.py
+++ tests/unit_test/test_signup.py
@@ -8,28 +8,26 @@ def test_new_signup():
     # asserting database is clear or has a length of 0
     assert len(list(UserTable.query.all())) is 0
     #create a new user
-    newuser = UserTable('obamna', 'soda',
+    newuser1 = UserTable('obamna', 'soda',
                         'The Barock', '123',
                         '44@gmail.com',
                         '123-456-7890')
     #add the new user
-    db.session.add(newuser)
+    db.session.add(newuser1)
     #commit
     db.session.commit()
 
-
-    Users = list(UserTable.query.all())
     #assert that a new user is added, length of 1
     assert len(list(UserTable.query.all())) == 1
-    newuser = UserTable('Crazy', 'guy',
+    newuser2 = UserTable('Crazy', 'guy',
                         'Crazy guy', '123',
                         'ex@gmail.com',
                         '123-456-1230')
-    db.session.add(newuser)
-    Users = list(UserTable.query.all())
+    db.session.add(newuser2)
     db.session.commit()
+    #assert the length of usertable is now 2
+    assert len(list(UserTable.query.all())) == 2
     #test after session commit
-    Users = UserTable.query.all()
-    assert 'Crazy guy' in str(Users)
+    assert 'Crazy guy' in str(UserTable.query.all())
     
-
+    #newuser 2 
